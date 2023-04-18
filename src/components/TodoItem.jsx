6f2d452e6b38d12b2e23accc7497559772935c25
Giovanny Vargas <yovaz05@gmail.com>
2023-04-18 18:53:27 -0500

    Clase 18-04-2023

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,12 +1,19 @@
 import styles from './TodoItem.module.css';
+import { FaEdit, FaTrash, FaCheck } from 'react-icons/fa';
 const TodoItem = ({ todo }) => {
 	return (
 		<li className={styles.list}>
 			<div className={styles.listItem}>{todo.title}</div>
 			<div>
-				<button>Edit</button>
-				<button>Delete</button>
-				<button>Check</button>
+				<button>
+					<FaEdit />
+				</button>
+				<button>
+					<FaTrash />
+				</button>
+				<button>
+					<FaCheck />
+				</button>
 			</div>
 		</li>
 	);
